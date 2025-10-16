dfa7a9e2de34e50da61b752e75ea5ca44012040b
LiyuanchengDD <liyuancheng980@gmail.com>
2025-10-16 21:46:32 +0900

    feat: 本次修改的说明

--- src/main/java/com/example/grpcdemo/health/ReflectionService.java
+++ src/main/java/com/example/grpcdemo/health/ReflectionService.java
@@ -8,7 +8,7 @@ import net.devh.boot.grpc.server.service.GrpcService;
 @GrpcService
 public class ReflectionService implements BindableService {
 
-    private final ProtoReflectionService delegate = ProtoReflectionService.newInstance();
+    private final BindableService delegate = ProtoReflectionService.newInstance();
 
     @Override
     public ServerServiceDefinition bindService() {
