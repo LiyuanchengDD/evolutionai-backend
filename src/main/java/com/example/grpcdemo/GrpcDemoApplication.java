eff95490a0bb57c2703d3eaf697f28c4e64aad78
LiyuanchengDD <liyuancheng980@gmail.com>
2025-10-17 20:49:28 +0900

    feat: 本次修改的说明

--- src/main/java/com/example/grpcdemo/GrpcDemoApplication.java
+++ src/main/java/com/example/grpcdemo/GrpcDemoApplication.java
@@ -1,13 +1,22 @@
+// src/main/java/com/example/grpcdemo/GrpcDemoApplication.java
 package com.example.grpcdemo;
 
+import com.example.grpcdemo.auth.AuthManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(
+    basePackages = "com.example.grpcdemo",
+    excludeFilters = @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = AuthManager.class
+    )
+)
 public class GrpcDemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GrpcDemoApplication.class, args);
     }
 }
-
